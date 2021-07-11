744ffe03c2932fc4c36c3c8457b186eeb75b6637
lara <lara@vehicle4change.org>
2021-07-10 22:40:28 -0400

    added questions to quiz

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+//Questions for the quiz
+var questions = [
+    {
+      qTitle: "Which tag is an extension to HTML that can enclose any number of JavaScript",
+      choices: ["<SCRIPT>", "<BODY>", "<HEAD>", "<TITLE>"],
+      correctAnswer: "<SCRIPT>"
+    },
+    {
+      qTitle: "Which method of an Array object adds and/or removes elements from an array.",
+      choices: ["Reverse", "Shift", "Slice", "Splice"],
+      correctAnswer: "Splice"
+    },
+    {
+      qTitle: "Which of these is not used to loop?",
+      choices: ["for", "while", "foreach", "sequence"],
+      correctAnswer: "sequence"
+    },
+    {
+      qTitle: "Which of the following cannot be done with client-side JavaScript?",
+      choices: ["Validating a form", "Sending a form's contents by email", "Storing the form's contents to a database file on the server", "Responding to user events such as mouse clicks"],
+      correctAnswer: "Storing the form's contents to a database file on the server"
+    },
+    {
+      qTitle: "Which of the following are capabilities of functions in JavaScript?",
+      choices: ["Return a value", "Accept parameters and Return a value", "Accept parameters", "None of the above"],
+      correctAnswer: "Accept parameters"
+    },
+   
+  ];
