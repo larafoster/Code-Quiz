617aee25a890d4fa0ee591a022d0c5f78d3b90d7
lara <lara@vehicle4change.org>
2021-07-13 18:45:54 -0400

    pulled out the questions and put in separate js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,33 @@
-// created an array and passing the number, questions, options, and answers
-var questions = [{
-    questionAsked: "Which tag is an extension to HTML that can enclose any number of JavaScript",
-      choices: ["<SCRIPT>", "<BODY>", "<HEAD>", "<TITLE>"],
-      correctAnswer: 0
+// created an array for the questions, options, and answers
+var questions = [
+    {
+      title: "Which tag is an extension to HTML that can enclose any number of JavaScript",
+      choices: ["SCRIPT", "BODY", "HEAD", "TITLE"],
+      answer: "SCRIPT"
     },
     {
-      questionAsked: "Which method of an Array object adds and/or removes elements from an array.",
+      title: "Which method of an Array object adds and/or removes elements from an array.",
       choices: ["Reverse", "Shift", "Slice", "Splice"],
-      correctAnswer: 3
+      answer: "Splice"
     },
     {
-      questionAsked: "Which of these is not used to loop?",
+      title: "Which of these is not used to loop?",
       choices: ["for", "while", "foreach", "sequence"],
-      correctAnswer: 3
+      answer: "sequence"
     },
     {
-      questionAsked: "Which of the following cannot be done with client-side JavaScript?",
+      title: "Which of the following cannot be done with client-side JavaScript?",
       choices: ["Validating a form", "Sending a form's contents by email", "Storing the form's contents to a database file on the server", "Responding to user events such as mouse clicks"],
-      correctAnswer: 2
+      answer: "Storing the form's contents to a database file on the server"
     },
     {
-      questionAsked: "Which of the following are capabilities of functions in JavaScript?",
+      title: "Which of the following are capabilities of functions in JavaScript?",
       choices: ["Return a value", "Accept parameters and Return a value", "Accept parameters", "None of the above"],
-      correctAnswer: 2
-  }];
+      answer: "Accept parameters"
+    },
+   
+  ];
+
   
   var highScore = []; //Array containing user scores
   var questionCounter = 0; //Tracks question number
